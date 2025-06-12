6f8f0ab787c6016737f89147e5053a9528468c90
Yogesh Kumar <yogeshkumar.taikisha@gmail.com>
2025-06-12 15:41:38 +0530

    added event cards using composition api

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,44 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+</script>
+
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+  <div id="layout">
+    <header>
+      <div id="wrapper">
+        <nav> 
+          <RouterLink to="/">Home</RouterLink> |
+          <RouterLink to="/about">About</RouterLink>
+        </nav>
+      </div>
+    </header>
+    <RouterView />
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+#layout {
+  font-family: Avenir, Arial, Helvetica, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
 
-#nav {
+nav{
   padding: 30px;
 }
 
-#nav a {
+nav a {
   font-weight: bold;
   color: #2c3e50;
 }
 
-#nav a.router-link-exact-active {
+nav a.router-link-exact-active {
   color: #42b983;
+}
+
+h2 {
+  font-size: 20px;
 }
 </style>
