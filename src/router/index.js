f47cd676d390daa607f265b3a5c9a0951699fcce
Yogesh Kumar <yogeshkumar.taikisha@gmail.com>
2025-06-24 11:26:33 +0530

    made changes to run

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/EventLIstView.vue";
+// import Home from "../views/EventLIstView.vue";
 import EventLIstView from "../views/EventLIstView.vue";
 // import Home from "../views/Home.vue";
 
